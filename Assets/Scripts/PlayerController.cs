4b90fb1bf694f455f66e4f1d0ef1714b3e6362a6
pablovass <faktory.fm@live.com>
2021-09-21 16:29:06 -0300

    juego sin terminar

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Range(0,20)]
+    [SerializeField]
+    [Tooltip("velocidad max del coche")]
+    private float speed=5f;
+    
+    [Range(0,90)]
+    [SerializeField]
+    [Tooltip("velocisdad maxima del coche")]
+    private float turnSpeed=50f;
 
-    // Update is called once per frame
+    private float HorizontalInput, verticalInput;
+    
     void Update()
     {
+        HorizontalInput = Input.GetAxis("Horizontal");
+        verticalInput= Input.GetAxis("Vertical");
+        //Debug.Log("MOVIMIENTO HORIZONTAL: "+HorizontalInput);
+        this.transform.Translate(speed*Time.deltaTime*Vector3.forward*verticalInput);
+        this.transform.Rotate(turnSpeed*Time.deltaTime*Vector3.up*HorizontalInput);
         
-    }
+    }  
 }
+
